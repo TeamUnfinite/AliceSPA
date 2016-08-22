8054c69078b05ebca2f99a2fe6c370ba6c349b56
lilei <lilei@haomo-studio.com>
2016-08-22 16:43:28 +0800

    n

--- Server/middleware/load.php
+++
@@ -1,4 +0,0 @@
-<?php
-$MIDDLEWARE_PATH = $SERVER_PATH . '/Middleware';
-require $MIDDLEWARE_PATH . '/Authentication.php';
-require $MIDDLEWARE_PATH . '/APIProtocol.php';//Attention for the loading index of APIProtocol , so that it could catch exception instances. e.g. APIException
