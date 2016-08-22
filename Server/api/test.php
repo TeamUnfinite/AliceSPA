95f0b7df741de7969d0ec13f6035145826077cb9
lilei <lilei@haomo-studio.com>
2016-08-22 16:27:07 +0800

    n

--- Server/api/test.php
+++ Server/api/test.php
@@ -1,37 +1,3 @@
 <?php
-use \AliceSPA\exception\APIException as apie;
-use \AliceSPA\service\VerificationCodeManager as vcode;
-use \AliceSPA\service\authentication as authService;
-$app->post('/api/test/auth',function($req,$res,$args){
-    //$this->apip->setDisabled();
-    $body = $req->getParsedBody();
 
-    //$body = $this->auth->loginByUnionField($body,$body['password']);
-    //$this->apip->setData($body);
-});
-$app->get('/api/test/vcode',function($req,$res,$args){
-    $this->apip->setDisabled();
-    $vcode = vcode::getInstance();
-    //$vcode->store(123,'123','email');
-    echo json_encode($vcode->clean(123,'email',1));
-});
-$app->get('/api/test/empty',function($req,$res,$args){
-	
-$this->apip->setDisabled();
-    echo json_encode($this->auth->registerByUnionField(['user_name'=>'a3','telephone' => '123'],'a'));
-});
-
-$app->post('/api/test/account/register',function($req,$res,$args){
-    $this->apip->setDisabled();
-});
-$app->post('/api/test/account/login',function($req,$res,$args){
-    $this->apip->setDisabled();
-    $parsedBody = $req->getParsedBody();
-
-    $r = $this->auth->loginByUnionField($parsedBody,$parsedBody['password']);
-    echo json_encode($r); 
-});
-$app->get('/api/test',function($req,$res,$args){
-    $this->apip->setData($this->db->select('account','*'));
-});
 
